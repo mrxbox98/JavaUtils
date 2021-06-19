f632abaf70d0b951891c2ac92a5ea0cf99cd2a67
Saahil Thoppay <23saahilt@students.harker.org>
2021-06-18 19:16:37 -0700

    Added Vec2i

---
+++ src/me/mrxbox98/vec/Vec2i.java
@@ -0,0 +1,50 @@
+package me.mrxbox98.vec;
+
+public class Vec2i {
+
+    private int x;
+
+    private int y;
+
+    public Vec2i(int x, int y)
+    {
+        this.x=x;
+        this.y=y;
+    }
+
+    /**
+     * Gets the X value
+     *
+     * @return The X value
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Gets the Y value
+     *
+     * @return The Y value
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * Sets the X value of the vec
+     *
+     * @param x the new X value
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * Sets the Y value of the vec
+     *
+             * @param y the new Y value
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+}
