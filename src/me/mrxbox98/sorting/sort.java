0b6df26b96d35b059cc8cc163cb517377d6449dc
Saahil Thoppay <23saahilt@students.harker.org>
2021-06-19 18:23:01 -0700

    Sort fix

---
+++ src/me/mrxbox98/sorting/sort.java
@@ -0,0 +1,36 @@
+package me.mrxbox98.sorting;
+
+import java.util.ArrayList;
+
+public class sort {
+
+    public static ArrayList<Integer> sortInteger(ArrayList<Integer> array)
+    {
+        for(int i=0;i<array.size();i++)
+        {
+            for(int x=0;x<array.size();x++)
+            {
+                if(i>x)
+                {
+                    if(array.get(i)<array.get(x))
+                    {
+                        Integer temp=array.get(i);
+                        array.set(i,array.get(x));
+                        array.set(x,temp);
+                    }
+                }
+                if(x>i)
+                {
+                    if(array.get(x)<array.get(i))
+                    {
+                        Integer temp=array.get(x);
+                        array.set(x,array.get(i));
+                        array.set(i,temp);
+                    }
+                }
+            }
+        }
+        return array;
+    }
+
+}
