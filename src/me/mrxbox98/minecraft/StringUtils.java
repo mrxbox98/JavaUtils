bb99a8b0251335507d3cd338b10a4a5f23784946
Saahil Thoppay <23saahilt@students.harker.org>
2021-06-30 23:07:05 -0700

    Changed some stuff

--- src/me/mrxbox98/minecraft/StringUtils.java
+++ src/me/mrxbox98/minecraft/StringUtils.java
@@ -4,28 +4,53 @@ import java.util.ArrayList;
 
 public class StringUtils {
 
-    public static final String BLACK="§0";
-    public static final String DARK_BLUE="§1";
-    public static final String DARK_GREEN="§2";
-    public static final String DARK_AQUA="§3";
-    public static final String DARK_RED="§4";
-    public static final String DARK_PURPLE="§5";
-    public static final String GOLD="§6";
-    public static final String GRAY="§7";
-    public static final String DARK_GRAY="§8";
-    public static final String BLUE="§9";
-    public static final String GREEN="§a";
-    public static final String AQUA="§b";
-    public static final String RED="§c";
-    public static final String LIGHT_PURPLE="§d";
-    public static final String YELLOW="§e";
-    public static final String WHITE="§f";
-    public static final String OBFUSCATED="§k";
-    public static final String BOLD="§l";
-    public static final String STRIKETHROUGH="§m";
-    public static final String UNDERLINE="§n";
-    public static final String ITALIC="§o";
-    public static final String RESET="§r";
+    public static final String BLACK;
+    public static final String DARK_BLUE;
+    public static final String DARK_GREEN;
+    public static final String DARK_AQUA;
+    public static final String DARK_RED;
+    public static final String DARK_PURPLE;
+    public static final String GOLD;
+    public static final String GRAY;
+    public static final String DARK_GRAY;
+    public static final String BLUE;
+    public static final String GREEN;
+    public static final String AQUA;
+    public static final String RED;
+    public static final String LIGHT_PURPLE;
+    public static final String YELLOW;
+    public static final String WHITE;
+    public static final String OBFUSCATED;
+    public static final String BOLD;
+    public static final String STRIKETHROUGH;
+    public static final String UNDERLINE;
+    public static final String ITALIC;
+    public static final String RESET;
+
+    static {
+        BLACK = "§0";
+        DARK_BLUE = "§1";
+        DARK_GREEN = "§2";
+        DARK_AQUA = "§3";
+        DARK_RED = "§4";
+        DARK_PURPLE = "§5";
+        GOLD = "§6";
+        GRAY = "§7";
+        DARK_GRAY = "§8";
+        BLUE = "§9";
+        GREEN = "§a";
+        AQUA = "§b";
+        RED = "§c";
+        LIGHT_PURPLE = "§d";
+        YELLOW = "§e";
+        WHITE = "§f";
+        OBFUSCATED = "§k";
+        BOLD = "§l";
+        STRIKETHROUGH = "§m";
+        UNDERLINE = "§n";
+        ITALIC = "§o";
+        RESET = "§r";
+    }
 
     /**
      * Converts multiple lines into a block of lines so that
