f6676976a259a8a987f53bb2841d2774492b85f6
FyreHost <23saahilt@students.harker.org>
2021-07-29 22:29:30 -0700

    Update Console.java

--- src/main/java/me/mrxbox98/logger/Console.java
+++ src/main/java/me/mrxbox98/logger/Console.java
@@ -2,39 +2,91 @@ package me.mrxbox98.logger;
 
 public interface Console {
 
+    /**
+     * Back ANSI escape code
+     */
     String Black = "\u001b[30m";
 
+    /**
+     * Red ANSI escape code
+     */
     String Red = "\u001b[31m";
 
+    /**
+     * Green ANSI escape code
+     */
     String Green = "\u001b[32m";
 
+    /**
+     * Yellow ANSI escape code
+     */
     String Yellow = "\u001b[33m";
 
+    /**
+     * Blue ANSI escape code
+     */
     String Blue = "\u001b[34m";
 
+    /**
+     * Magenta ANSI escape code
+     */
     String Magenta = "\u001b[35m";
 
+    /**
+     * Cyan ANSI escape code
+     */
     String Cyan = "\u001b[36m";
 
+    /**
+     * White ANSI escape code
+     */
     String White = "\u001b[37m";
 
+    /**
+     * Reset ANSI
+     */
     String Reset = "\u001b[0m";
 
+    /**
+     * Bright Black ANSI escape code
+     */
     String Bright_Black = "\u001b[30;1m";
 
+    /**
+     * Bright Red ANSI escape code
+     */
     String Bright_Red = "\u001b[31;1m";
 
+    /**
+     * Bright Green ANSI escape code
+     */
     String Bright_Green = "\u001b[32;1m";
 
+    /**
+     * Bright Yellow ANSI escape code
+     */
     String Bright_Yellow = "\u001b[33;1m";
 
+    /**
+     * Bright Blue ANSI escape code
+     */
     String Bright_Blue = "\u001b[34;1m";
 
+    /**
+     * Bright Magenta ANSI escape code
+     */
     String Bright_Magenta = "\u001b[35;1m";
 
+    /**
+     * Bright Cyan ANSI escape code
+     */
     String Bright_Cyan = "\u001b[36;1m";
 
+    /**
+     * Bright White ANSI escape code
+     */
     String Bright_White = "\u001b[37;1m";
+
 
 
 
